ed87b75f3e028d4f15a0748c39e923e395f27cc8
Maxime Collet <maxcollet01@gmail.com>
2026-01-15 21:08:00 +0100

    implementing the first level of security of the app + list of remaining todo's

--- backend/app.py
+++ backend/app.py
@@ -3,20 +3,48 @@ from firebase_admin import  auth
 from dotenv import load_dotenv
 import os
 
+# Additional Security Considerations:
+# TODO  Input Validation: Ensure that all user inputs are validated and sanitized.
+# TODO  Error Handling: Implement robust error handling to prevent sensitive information from being exposed.
+# TODO  HTTPS: Ensure that all communications with the server are encrypted using HTTPS.
+# TODO Rate Limiting: Implement rate limiting to prevent abuse of your API.
+# TODO  CSRF Protection: Use CSRF tokens if you're handling forms or other POST requests.
+
 load_dotenv()
 
 # ====== APP SET UP ===== #
 app = Flask(__name__, template_folder="../frontend/templates", static_folder="../frontend/static")
 
 SECRET_KEY = os.getenv("MY_SERVER_STAMP")
-app.secret_key = SECRET_KEY
+
+
 
 # ===== SECURITY OF THE SERVER ===== #
+app.secret_key = SECRET_KEY
+app.config["SESSION_USE_SIGNER"] = True # apply the stamp to the session
+app.config["SESSION_TYPE"] = "filesystem" # storage of the session (proto = server)
+app.config["SESSION_PERMANENT"] = False # when browser is closed then the session disapear
+app.config["SESSION_COOKIE_HTTPONLY"] = True # prevent XSS
+app.config["SESSION_COOKIE_SAMESITE"] = "Lax" # prevent CSRF
+
 
 # ===== SECURITY OF THE CLIENT (HEADER) ===== #
 @app.before_request
-def header_definition():
-    pass
+def header_security_definition(response):
+    response.headers["Content-Security-Policy"] = (
+        "default-src 'self'; "
+        "script-src 'self'; "
+        "style-src 'self' https://cdnjs.cloudflare.com https://fonts.googleapis.com; "
+        "font-src 'self' https://fonts.gstatic.com https://cdnjs.cloudflare.com; "
+        "img-src 'self' data:; "
+        "object-src 'none'; "
+        "base-uri 'self';"
+    )
+    response.headers["X-Content-Type-Options"] = "nosniff"
+    response.headers["X-Frame-Options"] = "DENY"
+    response.headers["Referrer-Policy"] = "strict-origin-when-cross-origin"
+    return response
+
 # ===== ROUTES ===== #
 # Home page
 @app.route("/")
