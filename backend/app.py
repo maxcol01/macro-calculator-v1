1624bea5449a5b409e00ddf415a5ad3d79797f87
Maxime Collet <maxcollet01@gmail.com>
2026-01-15 20:54:14 +0100

    adding the secret key generate randomly for the app stamp

--- backend/app.py
+++ backend/app.py
@@ -1,14 +1,22 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 from firebase_admin import  auth
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 # ====== APP SET UP ===== #
 app = Flask(__name__, template_folder="../frontend/templates", static_folder="../frontend/static")
 
+SECRET_KEY = os.getenv("MY_SERVER_STAMP")
+app.secret_key = SECRET_KEY
 
 # ===== SECURITY OF THE SERVER ===== #
 
 # ===== SECURITY OF THE CLIENT (HEADER) ===== #
-
+@app.before_request
+def header_definition():
+    pass
 # ===== ROUTES ===== #
 # Home page
 @app.route("/")
